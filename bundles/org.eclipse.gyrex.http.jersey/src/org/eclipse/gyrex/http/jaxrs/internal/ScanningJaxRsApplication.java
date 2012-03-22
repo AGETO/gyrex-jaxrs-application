7319df7b32aaf013c19d1019af013080c9de328a
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-03-22 10:35:24 +0100

    add support for injection of OSGi services into resources and providers
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- bundles/org.eclipse.gyrex.http.jersey/src/org/eclipse/gyrex/http/jaxrs/internal/ScanningJaxRsApplication.java
+++ bundles/org.eclipse.gyrex.http.jersey/src/org/eclipse/gyrex/http/jaxrs/internal/ScanningJaxRsApplication.java
@@ -21,9 +21,12 @@ import com.sun.jersey.api.core.ResourceConfig;
 public final class ScanningJaxRsApplication extends JaxRsApplication {
 
 	private final ResourceConfig resourceConfig;
+	private ServiceInjectableProvider serviceInjector;
+	private final Bundle bundle;
 
 	public ScanningJaxRsApplication(final String id, final IRuntimeContext context, final Bundle bundle) {
 		super(id, context);
+		this.bundle = bundle;
 
 		// create a scanning resource configuration which scans the bundle
 		// for all possible resources
@@ -32,6 +35,34 @@ public final class ScanningJaxRsApplication extends JaxRsApplication {
 
 	@Override
 	protected javax.ws.rs.core.Application createJaxRsApplication() {
+		if (null != serviceInjector) {
+			resourceConfig.getSingletons().add(serviceInjector);
+		}
+
 		return resourceConfig;
+	}
+
+	@Override
+	protected void doDestroy() {
+		if (null != serviceInjector) {
+			serviceInjector.dispose();
+		}
+		try {
+			// call super
+			super.doDestroy();
+		} finally {
+			if (null != serviceInjector) {
+				serviceInjector.dispose();
+				serviceInjector = null;
+			}
+		}
+	}
+
+	@Override
+	protected void doInit() throws IllegalStateException, Exception {
+		// create service injector
+		serviceInjector = new ServiceInjectableProvider(bundle.getBundleContext());
+		// call super
+		super.doInit();
 	}
 }
---
+++ bundles/org.eclipse.gyrex.http.jersey/src/org/eclipse/gyrex/http/jaxrs/internal/ServiceInjectableProvider.java
@@ -0,0 +1,126 @@
+/**
+ * Copyright (c) 2012 Gunnar Wagenknecht and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ */
+package org.eclipse.gyrex.http.jaxrs.internal;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.ext.Provider;
+
+import org.eclipse.gyrex.common.services.BundleServiceHelper;
+import org.eclipse.gyrex.common.services.IServiceProxy;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.jersey.core.spi.component.ComponentContext;
+import com.sun.jersey.core.spi.component.ComponentScope;
+import com.sun.jersey.spi.inject.Injectable;
+import com.sun.jersey.spi.inject.InjectableProvider;
+
+/**
+ * A simple provider which injects and OSGi service.
+ */
+@Provider
+public class ServiceInjectableProvider implements InjectableProvider<Context, Type> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ServiceInjectableProvider.class);
+
+	private final BundleContext bundleContext;
+	private final BundleServiceHelper serviceHelper;
+
+	/**
+	 * Creates a new instance.
+	 */
+	public ServiceInjectableProvider(final BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+		serviceHelper = new BundleServiceHelper(bundleContext);
+	}
+
+	public void dispose() {
+		serviceHelper.dispose();
+	}
+
+	private Class<?> getClass(final Type type) {
+		if (type instanceof Class<?>) {
+			return (Class<?>) type;
+		} else if (type instanceof ParameterizedType) {
+			try {
+				return (Class<?>) ((ParameterizedType) type).getRawType();
+			} catch (final Exception ignored) {
+				if (LOG.isDebugEnabled()) {
+					LOG.debug("Unable to cast raw type of ParameterizedType ({}) to Class.", type, ignored);
+				}
+			}
+		}
+		return null;
+	}
+
+	private Class<?> getCollectionElementType(final Type type) {
+		// assume that the element is a ParameterizedType
+		if (type instanceof ParameterizedType) {
+			final Type[] typeArguments = ((ParameterizedType) type).getActualTypeArguments();
+			// Collections can only be parameterized with one argument
+			if (typeArguments.length == 1) {
+				return getClass(typeArguments[0]);
+			}
+		}
+
+		if (LOG.isDebugEnabled()) {
+			LOG.debug("Unable to detect collection element type for ({}).", type);
+		}
+		return null;
+	}
+
+	@Override
+	public Injectable getInjectable(final ComponentContext componentContext, final Context contextAnnotation, final Type requestedType) {
+		final Class<?> clazz = getClass(requestedType);
+		if (null == clazz) {
+			return null;
+		}
+
+		// check if we have a collection
+		final boolean collectionOfServices = Collection.class.isAssignableFrom(clazz) || List.class.isAssignableFrom(clazz);
+
+		// detect OSGi service interface
+		final Class<?> serviceInterface = collectionOfServices ? getCollectionElementType(requestedType) : clazz;
+
+		// quck check if there is a service available
+		final ServiceReference<?> sr = bundleContext.getServiceReference(serviceInterface.getName());
+		if (sr == null) {
+			return null;
+		}
+
+		// a service reference is available
+		// return a proxy to decouple from the service life-cycle
+		final IServiceProxy<?> serviceProxy = serviceHelper.trackService(serviceInterface);
+		return new Injectable<Object>() {
+			@Override
+			public Object getValue() {
+				return serviceProxy.getProxy();
+			}
+		};
+	}
+
+	@Override
+	public ComponentScope getScope() {
+		// OSGi services are valid per request only
+		return ComponentScope.PerRequest;
+	}
+
+}
